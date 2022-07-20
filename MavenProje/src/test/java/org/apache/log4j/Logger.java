7b531b9cbef55e3254a7eca31e85420308efce7d
gmel <gultekin.melike784@gmail.com>
2022-07-20 22:32:49 +0300

    initial commit

--- MavenProje/src/test/java/org/apache/log4j/Logger.java
+++ MavenProje/src/test/java/org/apache/log4j/Logger.java
@@ -32,41 +32,23 @@ public class Logger {
         driver.findElement(By.id("search-input")).sendKeys("roman");
         driver.findElement(By.cssSelector("#product-577905")).click();
         driver.findElement(By.cssSelector("#search > span")).click();
-        //sepet ikonundaki deðeri kontrol et
         String a = driver.findElement(By.id("cart-items")).getText();
         String b =driver.findElement(By.id("button-cart")).getText();
         Assert.assertEquals(a,b, "Deðerler eþit deðil");
-        
         driver.findElement(By.cssSelector("#button-cart > span")).click();
         driver.findElement(By.id("cart-item")).sendKeys("#button-cart > span");
         driver.findElement(By.cssSelector("#cart > div.heading > div:nth-child(2) > h4")).click();
         driver.findElement(By.cssSelector("#js-cart")).click();
-        //ürünü bir miktar arttýr
-        String deger = driver.findElement(By.cssSelector("#cart_module > div.cart-info > table > tbody > tr > td.quantity > form > input[type=text]:nth-child(1)")).getText();
-        int value = value++;
-        deger = value;
-        
-        
-        //yenile butonu
-    	driver.findElement(By.cssSelector("#cart_module")).click();
-    	
-    	//sepet güncelle yazýsý kontrol
-    	String element = "Sepetiniz güncellendi";//beklenen mesaj
+        driver.findElement(By.cssSelector("#cart_module")).click();
+    	String element = "Sepetiniz güncellendi";
     	String mesaj = driver.findElement(By.cssSelector("#swal-toast > div > div")).getText();//gelen mesaj
     	Assert.assertEquals(mesaj, element, "kontrol baþarýsýz");
-   
-        //çarpýya basýlýr
-        
         driver.findElement(By.cssSelector("#cart_module > div.cart-info > table > tbody > tr > td.remove > a > i")).click();
         String element1="sepetiniz boþ";
         String mesaj1 = driver.findElement(By.cssSelector("#cart-items-empty")).getText();
         Assert.assertEquals(mesaj1, element1, "kontrol baþarýsýz");
         
-        
-       /* driver.findElement(By.id("btnGirisYap")).click();
-        logger.info("Yönlendirilen Sayfa : " + driver.getTitle());
-        String actResult = "Kullanýcý Tanýmlama";
-        Assert.assertEquals(actResult, driver.getTitle());*/
+       
     }
     
     @BeforeTest  
